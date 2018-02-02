4578f2fa1eb7cc9149295facce367b6baf0d5859
vivek.13462 <vivek.13462@gmail.com>
2018-02-01 22:00:54 -0800

    HAndling errors with try catch

--- frontend/src/app/web.service.ts
+++ frontend/src/app/web.service.ts
@@ -1,22 +1,38 @@
 import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/toPromise';
+import { MatSnackBar } from '@angular/material';
 
 @Injectable()
 export class WebService {
     BASE_URL = 'http://localhost:52835/api';
     messages = [];
-    constructor(private http: Http) {
+    constructor(private http: Http, private sb: MatSnackBar) {
         this.getMessages();
     }
 
     async getMessages() {
-       var response =  await this.http.get(this.BASE_URL + '/messages').toPromise();
-       this.messages = response.json();
+        try {
+            var response =  await this.http.get(this.BASE_URL + '/messages').toPromise();
+            this.messages = response.json();   
+        } catch (error) {
+           this.handleError("Unable to get messages");  
+        }
+       
     }
 
     async postMessage(message) {
-        var response = await this.http.post(this.BASE_URL + '/messages', message).toPromise();
-        this.messages.push(response.json());
+        try {
+            var response = await this.http.post(this.BASE_URL + '/messages', message).toPromise();
+            this.messages.push(response.json());   
+        } catch (error) {
+            this.handleError("Unable to Post message");  
+        }
+        
+    }
+
+    private handleError(error) {
+        console.error(error);
+        this.sb.open(error, 'close', {duration: 2000});
     }
 }
